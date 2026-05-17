c2f61793c01a6229528f23dd86f60da350581461
anvartalic <anvar.talic@hotmail.co.uk>
2026-05-17 22:34:18 +0200

    chore: remove all local microsoft development certificates

--- gorilla.client/vite.config.ts
+++ gorilla.client/vite.config.ts
@@ -2,18 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig(() => {
-    return {
-        // Completely remove mkcert from the plugins array here
-        plugins: [
-            react()
-        ],
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url))
-            }
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         }
-        // We completely remove the "server" block. 
-        // Vercel handles serving and production routing automatically.
     }
 })
